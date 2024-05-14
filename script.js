af5fe7f0ad55be1f84fa1dc05ae8f94259b95f73
noordinq <nooshqureish@gmail.com>
2024-05-14 15:15:59 +0300

    js full functionality added

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 const userInput = document.getElementById("user-input");
 const result = document.getElementById("results-div");
+const regex = /^(1\s?)?(\(\d{3}\)|\d{3})([\s\-]?)\d{3}([\s\-]?)\d{4}$/;
 
 document.getElementById("check-btn").addEventListener("click", () => {
   validateNumber();
 })
-
 document.getElementById("clear-btn").addEventListener("click", () => {
   result.textContent = "";
 })
 
-
 function validateNumber(str) {
+  let item = document.createElement("li");
+  result.appendChild(item);
 
   if (userInput.value === "") {
     alert("Please provide a phone number");
     return;
   }
 
+  if (regex.test(userInput.value)) {
+    result.textContent = "Valid US number: "+userInput.value;
+  } else {
+    result.textContent = "Invalid US number: "+userInput.value;
+  }
 }
+
+userInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    validateNumber();
+  }
+});
